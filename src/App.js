cfb6edb1770fe36ce53abd2a3b7fc6e03fa331d9
Kpritpal12 <kpritpal.12@gmail.com>
2022-04-27 21:11:52 +0530

    use-state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import ClassComp from "./FunctionComp/ClassComp"
-// import UseStateTest from './Lifecycle/UseStateTest'
-import { useState } from 'react'
-import UseEffectTest from './Lifecycle/UseEffectTest'
+import UseStateTest from './Lifecycle/UseStateTest'
+// import { useState } from 'react'
+// import UseEffectTest from './Lifecycle/UseEffectTest'
 const App= () =>{
 
-   const[showComp,setShow]=useState(true);
+   // const[showComp,setShow]=useState(true);
      return(
         <div>
            {/* <ClassComp/> */}
-           {/* <UseStateTest/> */}
+           <UseStateTest/>
            {/* <UseEffectTest/> */}
-           {showComp ? <UseEffectTest/> : null}
-           <button onClick={()=>setShow(false)}>unmount useffect comp</button>
+           {/* {showComp ? <UseEffectTest/> : null}
+           <button onClick={()=>setShow(false)}>unmount useffect comp</button> */}
         </div> 
      )
 }
